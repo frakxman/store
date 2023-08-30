c6e86e1ac19ee7e60355a312867788f720f84778
Fabian Rivera <frakxman@hotmail.com>
2023-08-30 15:11:16 -0500

    Create customer

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+
+  private fb = inject( FormBuilder );
+
+  public createUserForm: FormGroup = this.fb.group({
+    nit: ['', [ Validators.required ]],
+    digit: ['', [ Validators.required]],
+    document: ['', [ Validators.required ]],
+    firstName: ['', [ Validators.required, Validators.minLength(3) ]],
+    secondName: [''],
+    firstLastName: ['', [ Validators.minLength(3) ]],
+    secondLastName: ['', [ Validators.minLength(3) ]],
+    address: ['', []],
+    phone: ['', [ Validators.required, Validators.min(10) ]],
+    email: ['', [ Validators.required, Validators.email ]],
+    verificationEmail: ['', [ Validators.email ]],
+    confEmail: ['', [ Validators.email ]],
+    country: [''],
+    department: [''],
+    town: [''],
+    bill: ['', [ Validators.required ]]
+  });
+
+  createUser() {
+    console.log(this.createUserForm.value);
+  }
 
 }
