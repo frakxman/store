7a1672069b2b92b7f76bd28462356f819149662c
Fabian <frakxman@hotmail.com>
2023-09-01 18:22:32 -0500

    Categories component render

--- src/app/web/pages/categories/categories.component.ts
+++ src/app/web/pages/categories/categories.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { Categories } from '../../interfaces/categories.interfaces';
 import { CategoriesService } from '../../services/categories.service';
+import { ProductsService } from '../../services/products.service';
 
 
 @Component({
@@ -13,13 +14,23 @@ export class CategoriesComponent {
 
   public categories: Categories[] = [];
   
-  constructor( private categoriesServices: CategoriesService ) {}
+  constructor( private categoriesServices: CategoriesService, private productsService: ProductsService ) {}
 
   ngOnInit(): void {
       this.categoriesServices.getAllCategories()
         .subscribe( data => {
           this.categories = data;
         });
+  }
+
+  categorySelected( index: number ) {
+    let categoriesNames = this.categories.map((category) => category.nombre );
+    console.log(categoriesNames, index);
+    for( let i = 0; i <= this.categories.length; i++ ) {
+      if ( index === i ) {
+        this.productsService.getByCategory()
+      }
+    }
   }
 
 }
